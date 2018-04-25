0e647497b63f857028ff924d0313e9903024104d
driftfloat <fallwater@126.com>
2018-04-25 21:15:40 +0800

    home 180425

--- src/main/java/javatest/TestOptional.java
+++ src/main/java/javatest/TestOptional.java
@@ -6,7 +6,7 @@ public class TestOptional {
 
 	public static void main(String[] args) {
 		Optional< String > fullName = Optional.ofNullable( null );
-		System.out.println( "Full Name is set? " + fullName.isPresent() );        
+		System.out.println( "Full Name is set? " + fullName.isPresent() );  
 		System.out.println( "Full Name: " + fullName.orElseGet( () -> "[none]" ) ); 
 		System.out.println( fullName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
 	}
---
+++ src/main/java/spring/boot/Application.java
@@ -0,0 +1,20 @@
+package spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "hello";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
