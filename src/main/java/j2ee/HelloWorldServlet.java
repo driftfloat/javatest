106836acba548c4d8d4832c4c26d318b3e69746c
driftfloat <fallwater@126.com>
2018-05-14 23:00:55 +0800

    20180513

--- src/main/java/j2ee/HelloWorldServlet.java
+++ src/main/java/j2ee/HelloWorldServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 public class HelloWorldServlet extends HttpServlet {
 
 	private String message;
