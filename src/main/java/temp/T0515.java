018bd88675d8b79a0e395467e3a1d45239000ea9
driftfloat <fallwater@126.com>
2018-05-22 16:57:44 +0800

    add User email field

--- src/main/java/temp/T0515.java
+++ src/main/java/temp/T0515.java
@@ -3,6 +3,11 @@ package temp;
 public class T0515 {
 
 	public static void main(String[] args) {
+		String s = "test";
+		System.out.println(String.class.getClassLoader());
+		System.out.println(s.getClass().getClassLoader());
+		T0515 t0515 = new T0515();
+		System.out.println(T0515.class.getClassLoader());
 		int i = 1;
 		i = i++;
 		System.out.println(i);
