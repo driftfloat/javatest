f96ab92ce21609fdf33e63ab7466c6df2a56f39b
driftfloat <fallwater@126.com>
2018-10-25 13:37:28 +0800

    20181025

--- src/main/java/base/date/TestLocatDate.java
+++ src/main/java/base/date/TestLocatDate.java
@@ -10,17 +10,17 @@ public class TestLocatDate {
 		LocalDate date = LocalDate.now();
 		LocalTime time = LocalTime.now();
 		LocalDateTime today = LocalDateTime.now();
-		System.out.println(date);
-		System.out.println(time);
-		System.out.println(today);
+		System.out.println(date);		//	2018-10-24
+		System.out.println(time);		//	12:06:54.816
+		System.out.println(today);		//	2018-10-24T12:06:54.816
 		String now = LocalDateTime.now().toString().replace(":", "");
-		System.out.println(now);
+		System.out.println(now);		//	2018-10-24T120654.817
 		now = LocalDateTime.now().toString().replace(":", "").substring(0, 17);
-		System.out.println(now);
-		System.out.println(LocalDateTime.now().toString().replace("T", " ").substring(0, 19));
-		System.out.println(LocalDate.now().getYear());
-		System.out.println(LocalDate.now().getMonthValue());
-		System.out.println(LocalDate.now().getMonth());
+		System.out.println(now);		//	2018-10-24T120654
+		System.out.println(LocalDateTime.now().toString().replace("T", " ").substring(0, 19));	// 2018-10-24 12:06:54
+		System.out.println(LocalDate.now().getYear());			//	2018
+		System.out.println(LocalDate.now().getMonthValue());	//	10
+		System.out.println(LocalDate.now().getMonth());			//	OCTOBER
 	}
 
 }
